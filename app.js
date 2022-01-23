5869971245bcfe07bad1cf41f6a1ae30922c53d9
Daniel Kim <daniel.d.kim13@gmail.com>
2022-01-23 16:22:49 -0500

    filter and map for feature

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // const [name, github] = profileDataArgs;
 
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
 // const pageHTML = generatePage(name, github);
 
@@ -143,6 +143,12 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    console.log(portfolioData);
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
